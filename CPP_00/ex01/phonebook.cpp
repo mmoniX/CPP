68ef1f876fedd8254025482b7c02c4ab98f96db7
unknown <zabin.mm@gmail.com>
2025-06-01 18:53:58 +0200

    CPP00_ex01 without class

--- CPP_00/ex01/phonebook.cpp
+++ CPP_00/ex01/phonebook.cpp
@@ -1,10 +1,44 @@
 #include "phonebook.hpp"
 
+std::string format_char(std::string &str)
+{
+    if (str.length() > 10)
+        return (str.substr(0,9) + '.');
+    return str;
+}
+
 int main(void)
 {
-    ADD-> First Name, Last Name, Nickname, Phone Number, Darkest Secret; {NO EMPTY FIELDS}
-    SEARCH;
-    EXIT;
-
-    return (0);
+    std::string command;
+    Contact entry;
+    while (true)
+    {
+        std::cout << "Enter command (ADD, SEARCH, EXIT): ";
+        std::cin >> command;
+        if (command == "ADD")
+        {
+            std::cout << "Enter First Name: ";
+            std::cin >> entry.first_name;
+            std::cout << "Enter Last Name: ";
+            std::cin >> entry.last_name;
+            std::cout << "Enter Nickname: ";
+            std::cin >> entry.nickname;
+            std::cout << "Enter Phone Number: ";
+            std::cin >> entry.phone_number;
+            std::cout << "Enter The Darkest Secret: ";
+            std::cin >> entry.darkest_secret;
+            entry.index++;
+        }
+        if (command == "SEARCH")
+        {
+            std::cout << std::right << std::setw(10) << entry.index << " |" 
+                << std::right << std::setw(10) << format_char(entry.first_name) << " |" 
+                << std::right << std::setw(10) << format_char(entry.last_name) << " |" 
+                << std::right << std::setw(10) << format_char(entry.nickname) << std::endl;
+        }
+        if (command == "EXIT")
+            return (0);
+    }
+    // ADD-> First Name, Last Name, Nickname, Phone Number, Darkest Secret; {NO EMPTY FIELDS}
+    
 }
--- CPP_00/ex01/phonebook.hpp
+++ CPP_00/ex01/phonebook.hpp
@@ -1,1 +1,28 @@
-#include <iostream>
+#ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include <list>
+
+// list<std::string> Contract
+// Contract.push_back()
+// Contract.pop_front()
+// Contract.size() == 8
+class PhoneBook
+{
+    //array of 8 contacts
+}
+class Contact
+{
+    private:
+        std::string phone_number;
+        std::string darkest_secret;
+    public:
+        int index;
+        std::string first_name;
+        std::string last_name;
+        std::string nickname;
+};
+
+#endif
