1ac31688c527518ed6aeda3ed8f8c9ac86c64ac7
mmoniX <zabin.mm@gmail.com>
2025-07-14 14:16:57 +0200

    CPP_04: start

---
+++ CPP_04/ex00/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmonika <mmonika@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/14 14:15:09 by mmonika           #+#    #+#             */
+/*   Updated: 2025/07/14 14:16:08 by mmonika          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include ""
+int main( void )
+{
+	const Animal* meta = new Animal();
+	const Animal* j = new Dog();
+	const Animal* i = new Cat();
+	std::cout << j->getType() << " " << std::endl;
+	std::cout << i->getType() << " " << std::endl;
+	i->makeSound(); //will output the cat sound!
+	j->makeSound();
+	meta->makeSound();
+
+	return (0);
+}
