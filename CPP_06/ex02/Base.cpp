749ab457e4afc26f7b0b4c7a9a56a7be9f72eb48
Mahjabin Monika <mmonika@2-I-1.42heilbronn.de>
2026-03-29 13:29:11 +0200

    CPP_06: done

--- CPP_06/ex02/Base.cpp
+++ CPP_06/ex02/Base.cpp
@@ -6,7 +6,7 @@
 /*   By: mmonika <mmonika@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 14:19:17 by mmonika           #+#    #+#             */
-/*   Updated: 2026/03/28 16:07:36 by mmonika          ###   ########.fr       */
+/*   Updated: 2026/03/29 13:23:28 by mmonika          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,33 +31,40 @@ Base *generate(void)
 
 void identify(Base *p)
 {
+	if (!p)
+	{
+		std::cout << "Null pointer" << std::endl;
+		return;
+	}
 	if (dynamic_cast<A*>(p))
-		std::cout << "A\n";
+		std::cout << "A" << std::endl;
 	else if (dynamic_cast<B*>(p))
-		std::cout << "B\n";
+		std::cout << "B" << std::endl;
 	else if (dynamic_cast<C*>(p))
-		std::cout << "C\n";
+		std::cout << "C" << std::endl;
+	else
+		std::cout << "Unknown type" << std::endl;
 }
 
 void identify(Base &p)
 {
 	try {
 		(void)dynamic_cast<A&>(p);
-		std::cout << "A\n";
+		std::cout << "A" << std::endl;
 		return;
 	} catch (std::bad_cast &) {}
-
+	
 	try {
 		(void)dynamic_cast<B&>(p);
-		std::cout << "B\n";
+		std::cout << "B" << std::endl;
 		return;
 	} catch (std::bad_cast &) {}
-
+	
 	try {
 		(void)dynamic_cast<C&>(p);
-		std::cout << "C\n";
+		std::cout << "C" << std::endl;
 		return;
 	} catch (std::bad_cast &) {}
-
-	std::cerr << "DEBUG: Unknown type\n";
+	
+	std::cerr << "Unknown" << std::endl;
 }
--- CPP_06/ex02/main.cpp
+++ CPP_06/ex02/main.cpp
@@ -6,17 +6,23 @@
 /*   By: mmonika <mmonika@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/21 14:54:25 by mmonika           #+#    #+#             */
-/*   Updated: 2026/03/28 16:11:10 by mmonika          ###   ########.fr       */
+/*   Updated: 2026/03/29 13:17:19 by mmonika          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
 
+void test(Base obj)
+{
+	std::cout << "test(Base&): ";
+	identify(obj);
+}
+
 int main()
 {
 	std::srand(static_cast<unsigned int>(std::time(NULL)));
 
-	for (int i = 0; i < 7; ++i)
+	for (int i = 0; i < 5; ++i)
 	{
 		Base *p = generate();
 		std::cout << "identify(Base*): ";
@@ -26,5 +32,21 @@ int main()
 		delete p;
 	}
 
+	Base *p1 = new A;
+	std::cout << "(Base*): ";
+	identify(p1);
+	test(*p1);
+
+	std::cout << "\n--- Unknown tests ---\n";
+	Base base;
+	std::cout << "identify(Base&): ";
+	identify(base);
+	Base *pb = new Base;
+	std::cout << "identify(Base*): ";
+	identify(pb);
+	delete pb;
+	std::cout << "identify(Base*): ";
+	identify(NULL);
+	delete p1;
 	return (0);
 }
